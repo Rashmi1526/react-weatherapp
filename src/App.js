c7b69a716ccbdcb9451c0fbe542412f0a93ba9fb
Rashmi1526 <rashmi.19b101039@abes.ac.in>
2022-06-24 21:58:38 +0530

    changing some variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import UilReact from "@iconscout/react-unicons/icons/uil-react";
+//import UilReact from "@iconscout/react-unicons/icons/uil-react";
 import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
